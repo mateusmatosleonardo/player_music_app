75a95a99a5be3c0fd56079f0be5fe93716207b14
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-04 15:47:18 -0300

    :hammer: creating global type to the global theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,23 @@
+export const theme = {
+  name: "DefaultTheme",
+  colors: {
+    WHITE: "#eef2f5",
+    BLACK: "#313030",
+    PRIMARY: "#334153",
+    SECONDARY: "#4030ff",
+    GRAY: "#EEEEEE",
+  },
+  fonts: {
+    Montserrat_Regular: "Montserrat_400Regular",
+    Montserrat_Medium: "Montserrat_500Medium",
+    Montserrat_SemiBold: "Montserrat_600SemiBold",
+    Montserrat_Bold: "Montserrat_700Bold",
+  },
+  sizes: {
+    SMALL: 12,
+    NORMAL: 14,
+    MEDIUM_LARGE: 16,
+    LARGE: 20,
+    ULTRA_LARGE: 22,
+  },
+};
